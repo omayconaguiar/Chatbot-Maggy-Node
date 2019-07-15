b297ad16586221adba2f9f0b39c9641d664003d5
Maycon Aguiar <maayconaguiar11@hotmail.com>
2019-07-15 15:21:24 -0300

    funcao boleto feito

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api'); 
-const token = '888694914:AAGDAmfqBYVdf4oZxjurh3SDpixWUz_oOBk';
+const token = '#';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/start/, (msg, match) => {
+
+bot.onText(/start/, (msg, match) => {
     const opts = {
         reply_to_message_id: msg.message_id,
         reply_markup: JSON.stringify({
@@ -15,7 +16,42 @@ bot.onText(/\/start/, (msg, match) => {
     bot.sendMessage(msg.chat.id, 'Hi. I am a simple bot. You can get the current price and blockchain height. Have fun!', opts);
 });
 
-bot.on('text', async (ctx) => {
-    const remoteFile = await request('http://www.lehtml.com/download/js_doc.pdf')
-    return bot.telegram.sendDocument(process.env.CHAT_ID, remoteFile, [{disable_notification: true}]);
-  })
+bot.onText(/boleto/, (msg, match) => {
+    const url = 'http://www.pdf995.com/samples/pdf.pdf';
+    bot.sendDocument(544663315, url);
+    });
+    
+bot.onText(/dados/, (msg, match) => {
+    const opts = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+              [{
+                text: 'Telefone',
+                callback_data: 'Telefone'
+              }],
+              [{
+                text: 'Endereço',
+                callback_data: 'Endereço'
+              }],
+              [{
+                text: 'Email',
+                callback_data: 'Email'
+              }],
+            ],
+          }),
+      };
+      bot.sendMessage(msg.from.id, 'Escolha uma das opções abaixo', opts);
+    });
+    
+    bot.on('callback_query', function onCallbackQuery(example){
+        const action = example.data // This is responsible for checking the content of callback_data
+        const msg = example.message
+        
+        if (action == 'Telefone'){
+            bot.on('message', (msg) => {
+                const chatId = msg.chat.id;
+                // send a message to the chat acknowledging receipt of their message
+                bot.sendMessage(chatId, 'Received your message');
+              });
+        }
+        });
