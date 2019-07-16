dc3dc0cbf05549c242561ff46f74f5aa57dd7c8b
Maycon Aguiar <maayconaguiar11@hotmail.com>
2019-07-16 16:20:17 -0300

    alterar dados minimamente feito

--- bot.js
+++ bot.js
@@ -3,17 +3,17 @@ const token = '888694914:AAGDAmfqBYVdf4oZxjurh3SDpixWUz_oOBk';
 const bot = new TelegramBot(token, {polling: true});
 
 
-bot.onText(/start/, (msg, match) => {
+bot.on('message', (msg, match) => {
     const opts = {
-        reply_to_message_id: msg.message_id,
         reply_markup: JSON.stringify({
             keyboard: [
-                ['boleto'],
-                ['dados']
+                ['consultar boleto'],
+                ['alterar dados'],
+                ['forma de pagamento']
             ]
         })
     };
-    bot.sendMessage(msg.chat.id, 'Hi. I am a simple bot. You can get the current price and blockchain height. Have fun!', opts);
+    bot.sendMessage(msg.chat.id, 'Eu sou Maggy, a assistente virtual da mongeral! Em que posso ajudar?', opts);
 });
 
 bot.onText(/boleto/, (msg, match) => {
@@ -42,6 +42,24 @@ bot.onText(/dados/, (msg, match) => {
       };
       bot.sendMessage(msg.from.id, 'Escolha uma das opções abaixo', opts);
     });
+
+    bot.onText(/pagamento/, (msg, match) => {
+        const opts = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                [{
+                    text: 'Alterar para Boleto',
+                    callback_data: 'Alterar para Boleto'
+                }],
+                [{
+                    text: 'Alterar para Débito Automático',
+                    callback_data: 'Alterar para Débito Automático'
+                }],
+                ],
+            }),
+        };
+        bot.sendMessage(msg.from.id, 'Escolha uma das opções abaixo', opts);
+    });
     
     bot.on('callback_query', function onCallbackQuery(example){
         const action = example.data // This is responsible for checking the content of callback_data
@@ -49,8 +67,33 @@ bot.onText(/dados/, (msg, match) => {
         
         if (action == 'Telefone'){
             const chatId = msg.chat.id;
-            // send a message to the chat acknowledging receipt of their message
             bot.sendMessage(chatId, 'Quer mudar para qual número?');
-              
+            bot.on('message', (msg) => {
+                bot.sendMessage(chatId, 'Mudamos seu número senhor Maycon');
+              });
         }
-        });
+        else if (action == 'Endereço'){
+            const chatId = msg.chat.id;
+            bot.sendMessage(chatId, 'Vamos lá! Qual o seu novo endereço? No formato: (Rua, número, bairro, cidade)');
+            bot.on('message', (msg) => {
+                bot.sendMessage(chatId, 'Mudamos seu endereço senhor Maycon');
+            });
+        }
+        else if (action == 'Email'){
+            const chatId = msg.chat.id;
+            bot.sendMessage(chatId, 'Digite o email que você quer mudar');
+            bot.on('message', (msg) => {
+                const chatId = msg.chat.id;
+                bot.sendMessage(chatId, 'Mudamos seu email senhor Maycon');
+            });
+        }
+        else if (action == 'Alterar para Boleto'){
+            const chatId = msg.chat.id;
+            bot.sendMessage(chatId, 'Mudamos');
+        }
+        else{
+            const chatId = msg.chat.id;
+            bot.sendMessage(chatId, 'Mudamos');        
+        }
+
+    }); 
